9d441fdeedcbae9c836d034dc57b71cc439f638c
Sizwe Chris Khomo <cs.khomo@outlook.com>
2026-05-26 22:11:55 +0200

    variables

---
+++ variables.js
@@ -0,0 +1,27 @@
+const name = "Sizwe Chris Khomo";
+let age = 26;
+let isStudent = true;
+
+console.log(`Name: ${name}` + `Age: ${age}` + `Student: ${isStudent}`);
+
+
+let favoriteColour = "Black";
+console.log("Color:", favoriteColour);
+
+favoriteColour = "White";
+console.log("Color:", favoriteColour);
+
+
+const num1 = 1, num2 = 2;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log(`${num1} + ${num2} = ${sum}`);
+console.log(`${num1} - ${num2} = ${difference}`);
+console.log(`${num1} x ${num2} = ${product}`);
+console.log(`${num1} / ${num2} = ${quotient}`);
+
+
