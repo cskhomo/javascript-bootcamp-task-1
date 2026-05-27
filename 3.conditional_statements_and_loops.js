377f8ce0e9231b38aefc9cdee58651f2ff177499
Sizwe Chris Khomo <cs.khomo@outlook.com>
2026-05-27 05:32:50 +0200

    weather

---
+++ 3.conditional_statements_and_loops.js
@@ -0,0 +1,17 @@
+const temparature = 20
+
+if (temparature < 0) {
+    console.log('It is freezing!')
+}
+
+if (temparature >= 0 && temparature <= 15) {
+    console.log('It is cold')
+}
+
+if (temparature >= 16 && temparature <= 25) {
+    console.log('It is mild')
+}
+
+if (temparature > 25) {
+    console.log("It's warm")
+}
